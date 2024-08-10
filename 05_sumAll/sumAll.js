0b2c46838c1129a847e2da4820767ae1c283c330
r0k4s <mrokass@gmail.com>
2024-08-10 19:41:53 +0200

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a < 0 || b < 0 || !Number.isInteger(b) || !Number.isInteger(a)) {
+    return "ERROR";
+  }
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  }
+  if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
