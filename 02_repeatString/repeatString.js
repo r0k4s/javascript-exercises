2110038376f9342b5e35f01805d906004af70819
r0k4s <mrokass@gmail.com>
2024-08-10 11:44:24 +0200

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
-
+const repeatString = function (text) {
+  let word = "";
+  let number = 10;
+  if (number >= 0) {
+    text = word.repeat(number);
+  } else {
+    text = "ERROR";
+  }
+  return text;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
+
+/*
+const repeatString = function (text) {
+  let word = "goodbye";
+  let number = -1;
+
+  if (number >= 0) {
+    text = word.repeat(number);
+  } else {
+    text = "ERROR";
+  }
+  return text;
+};
+*/
