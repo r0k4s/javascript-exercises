c52925f947148a5995678b269d3289fb2f8b9cdf
r0k4s <mrokass@gmail.com>
2024-08-10 18:01:56 +0200

    commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (givenArray, ...parameters) {
+  const newArray = [];
+
+  for (let i = 0; i < givenArray.length; i++) {
+    if (!parameters.includes(givenArray[i])) {
+      newArray.push(givenArray[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
