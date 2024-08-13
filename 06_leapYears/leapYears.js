8e2a2d281256de2b47eef5acf7b420b8be8f5f0c
r0k4s <mrokass@gmail.com>
2024-08-13 22:50:32 +0200

    06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let value = "";
+  if (year % 4 === 0 && year % 100 !== 0) {
+    value = true;
+  } else if (year % 400 === 0) {
+    value = true;
+  } else {
+    value = false;
+  }
+  return value;
 };
 
 // Do not edit below this line
