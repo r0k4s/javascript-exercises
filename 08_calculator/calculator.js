853351381f7ea5f985e314d334962ff5dede625d
r0k4s <mrokass@gmail.com>
2024-08-14 22:59:18 +0200

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (obj) {
+  let sum = obj.reduce((acc, currVal) => acc + currVal, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (obj) {
+  let multiply = obj.reduce((acc, currVal) => acc * currVal, 1);
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (obj) {
+  let factorial = 1;
+  for (let i = 2; i <= obj; i++) factorial = factorial * i;
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
