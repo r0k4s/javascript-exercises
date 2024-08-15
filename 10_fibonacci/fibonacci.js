50e6f81537288908c3dcf0b80087a215640c8244
r0k4s <mrokass@gmail.com>
2024-08-15 22:37:33 +0200

    10 fuck

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
 
+  let a = 0,
+    b = 1,
+    temp;
+
+  for (let i = 2; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
 };
 
 // Do not edit below this line
