dba5a5c26e90689e5ee361c73020056a9d4ce986
r0k4s <mrokass@gmail.com>
2024-08-15 22:05:40 +0200

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,6 @@ const palindromes = function (string) {
   const cleanString = string
     .replace(/[\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
     .toLowerCase();
-  console.log(cleanString);
   return cleanString === cleanString.split("").reverse().join("");
 };
 
